7e53d397491255749c35b4edab81bfc27f135b80
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2013-08-20 22:48:38 -0300

    fixing ugly indentation

--- hcsr04/hcsr04.ino
+++ hcsr04/hcsr04.ino
@@ -11,33 +11,37 @@ Arduino UNO R3.
 
 const long g_distance = 4;
 
-void setup() {
-  	Serial.begin (9600);
-  	pinMode(trigPin, OUTPUT);
-  	pinMode(echoPin, INPUT);
-  	pinMode(led, OUTPUT);
-  	pinMode(led2, OUTPUT);
+void setup() 
+{
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
-void loop() {
-  	long duration, distance;
-  	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2); 
-  	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10); 
-  	digitalWrite(trigPin, LOW);
-  	duration = pulseIn(echoPin, HIGH);
-  	distance = (duration/2) / 29.1;
-  	turnLedByDistance(distance);
-  	delay(500);
+void loop() 
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  turnLedByDistance(distance);
+  delay(500);
 }
 
-void turnLedByDistance(long distance) {
-	if (distance < g_distance) {
-		digitalWrite(led, HIGH);
-		digitalWrite(led2, LOW);
-	} else {
-		digitalWrite(led, LOW);
-		digitalWrite(led2, HIGH);
-	}
+void turnLedByDistance(long distance) 
+{
+  if (distance < g_distance) 
+  {
+  	digitalWrite(led, HIGH);
+	digitalWrite(led2, LOW);
+  } else {
+	digitalWrite(led, LOW);
+	digitalWrite(led2, HIGH);
+  }
 }
